515a99c3f828c418afe31364ec092ce708193098
Pau Fernández <pau.fernandez@upc.edu>
2020-02-21 15:46:10 +0100

    Retoques generales en preparación del cambio a AFA

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The Header template for our theme
  *
@@ -8,7 +9,8 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
@@ -18,42 +20,49 @@
 <!--[if !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
-<!--[if lt IE 9]>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width" />
+  <title><?php wp_title('|', true, 'right'); ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+  <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. 
+  ?>
+  <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
-<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<header id="masthead" class="site-header" role="banner">
-		<hgroup style="position: relative">
-			<h1 class="site-title">
-             <!-- <img src="/wp-content/uploads/2017/07/logo-petit.jpg" style="width: 80px; padding-left: 25px"> -->
-             <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-         </h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-         <a class="twitter" href="https://twitter.com/ampapixandri">
-            <img src="/twitter.png">
-         </a>
-		</hgroup>
+  <div id="page" class="hfeed site">
+    <header id="masthead" class="site-header" role="banner">
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+      <hgroup style="position: relative">
+        <h1 class="site-title">
+          <img src="/logo-petit.jpg" />
+          <a class="title"
+             href="<?php echo esc_url(home_url('/')); ?>" 
+             title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home">
+            <?php bloginfo('name'); ?>
+          </a>
+          <a class="twitter" href="https://twitter.com/ampapixandri">
+            <img src="/twitter.svg">
+          </a>
+        </h1>
+        <h2 class="site-description"><?php bloginfo('description'); ?></h2>
+      </hgroup>
 
-		<?php if ( get_header_image() ) : ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
-		<?php endif; ?>
-	</header><!-- #masthead -->
+      <nav id="site-navigation" class="main-navigation" role="navigation">
+        <button class="menu-toggle"><?php _e('Menu', 'twentytwelve'); ?></button>
+        <a class="assistive-text" href="#content" title="<?php esc_attr_e('Skip to content', 'twentytwelve'); ?>"><?php _e('Skip to content', 'twentytwelve'); ?></a>
+        <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'nav-menu')); ?>
+      </nav><!-- #site-navigation -->
 
-	<div id="main" class="wrapper">
+      <?php if (get_header_image()) : ?>
+        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr(get_custom_header()->width); ?>" height="<?php echo esc_attr(get_custom_header()->height); ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" /></a>
+      <?php endif; ?>
+    </header><!-- #masthead -->
+
+    <div id="main" class="wrapper">
